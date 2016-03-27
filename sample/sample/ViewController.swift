f02ab1a71ef8dbb18e1c1c5c54f6cc3e8574bb6f
MikamiTakuya <mikamitakuya@Mikami-mbp.local>
2016-03-27 15:12:30 +0900

    Xcodeでアプリを新規作成

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by MikamiTakuya on 2016/03/27.
+//  Copyright © 2016年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
